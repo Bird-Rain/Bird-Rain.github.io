dacc7d54109c18847290f77b5ff4760f5ad6d65b
Bird-Rain <154965864+Bird-Rain@users.noreply.github.com>
2024-05-30 16:13:32 +0900

    Add files via upload

--- js/router.js
+++ js/router.js
@@ -1,26 +1,22 @@
 const route = (event) => {
     event = event || window.event;
     event.preventDefault();
-    const targetPath = event.target.getAttribute('href');
-    window.history.pushState({}, "", targetPath);
+    window.history.pushState({}, "", event.target.href);
     handleLocation();
 };
 
 const routes = {
-    "/": "/pages/home.html",
-    "/interested": "/pages/interested.html",
-    "/schedule": "/pages/schedule.html",
+    404: "/pages/404.html",
+    "/": "/pages/index.html",
+    "/about": "/pages/about.html",
+    "/lorem": "/pages/lorem.html",
 };
 
 const handleLocation = async () => {
     const path = window.location.pathname;
-    const route = routes[path];
-    if (route) {
-        const html = await fetch(route).then((data) => data.text());
-        document.getElementById("main-page").innerHTML = html;
-    }else{
-        document.getElementById("main-page").innerHTML = "<h2>404 - Page Not Found</h2>";
-    }
+    const route = routes[path] || routes[404];
+    const html = await fetch(route).then((data) => data.text());
+    document.getElementById("main-page").innerHTML = html;
 };
 
 window.onpopstate = handleLocation;
