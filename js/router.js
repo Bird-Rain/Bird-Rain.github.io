668b73830934f7630eca781d3172417628c74df4
Bird-Rain <154965864+Bird-Rain@users.noreply.github.com>
2024-05-30 15:44:05 +0900

    Add files via upload

--- js/router.js
+++ js/router.js
@@ -1,7 +1,8 @@
 const route = (event) => {
     event = event || window.event;
     event.preventDefault();
-    window.history.pushState({}, "", event.target.href);
+    const targetPath = event.target.getAttribute('href');
+    window.history.pushState({}, "", targetPath);
     handleLocation();
 };
 
@@ -15,7 +16,7 @@ const handleLocation = async () => {
     const path = window.location.pathname;
     const route = routes[path];
     const html = await fetch(route).then((data) => data.text());
-    document.getElementById("main").innerHTML = html;
+    document.getElementById("main-page").innerHTML = html;
 };
 
 window.onpopstate = handleLocation;
