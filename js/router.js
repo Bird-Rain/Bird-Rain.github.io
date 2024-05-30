c6197ab59048db62657dccc5da4d5ddb6eb278dc
Bird-Rain <154965864+Bird-Rain@users.noreply.github.com>
2024-05-30 18:26:59 +0900

    Add files via upload

--- js/router.js
+++ js/router.js
@@ -8,8 +8,8 @@ const route = (event) => {
 const routes = {
     404: "/pages/404.html",
     "/": "/pages/index.html",
-    "/about": "/pages/about.html",
-    "/lorem": "/pages/lorem.html",
+    "/inter": "/pages/inter.html",
+    "/sch": "/pages/sch.html",
 };
 
 const loadgallery = () => {
