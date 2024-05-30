5ef6c358d575f8ad1bd11dc5253a40a365cb5d16
Bird-Rain <154965864+Bird-Rain@users.noreply.github.com>
2024-05-30 18:17:47 +0900

    Add files via upload

--- js/router.js
+++ js/router.js
@@ -12,13 +12,41 @@ const routes = {
     "/lorem": "/pages/lorem.html",
 };
 
+const loadgallery = () => {
+    alert('Loading Gallery');
+    var width = $('[data-role="slider"]').attr('data-width');
+    var height = $('[data-role="slider"]').attr('data-height');
+    var count = $('[data-role="slider"] div.item').length;
+    $('[data-role="slider"]').css({
+        position: 'relative', overflow: 'hidden', width: width, height: height
+    })
+        .find('.container_slide').css({
+            position: 'absolute', width: count * width, overflow: 'hidden'
+        })
+        .find('.item').css({
+            width: width, height: height, float: 'left'
+        })
+
+    var currentPage = 0;
+    var changePage = function () {
+        $('[data-role="slider"] > .container_slide').animate({ left: -currentPage * width }
+            , 500);
+    };
+
+    $('#left-button').click(function () {
+        if (currentPage > 0) { currentPage--; changePage(); }
+    });
+    $('#right-button').click(function () {
+        if (currentPage < count - 1) { currentPage++; changePage(); }
+    });
+};
+
 const handleLocation = async () => {
     const path = window.location.pathname;
     const route = routes[path] || routes[404];
     const html = await fetch(route).then((data) => data.text());
     document.getElementById("main-page").innerHTML = html;
     loadgallery();
-    
 };
 
 
